3f05e49b4a2cd4acb8b1c2e6b9e7bc4dc85d9069
charttrakarn_ch <59467751+charttrakarn0784@users.noreply.github.com>
2022-08-04 14:15:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+const randBtn = document.getElementById("btn-random");
+const imgProflie = document.getElementById("img-profile");
+const profName = document.getElementById("p-name");
+const profEmail = document.getElementById("p-email");
+const profAddress = document.getElementById("p-address");
+
+randBtn.onclick = async () => {
+  const resp = await axios.get("https://randomuser.me/api/");
+  console.log(resp.data.results);
+  const result = resp.data.results;
+  imgProfile.src = result.picture.thumbnail;
+};
